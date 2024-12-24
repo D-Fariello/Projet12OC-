15deef14de40f26922079113df043d220d74a237
Dalila <dalila.fariello@gmail.com>
2024-12-24 13:12:00 +0100

    feat: refactor Projects component to dynamically render project cards from external data source

---
+++ src/data/projectsData.js
@@ -0,0 +1,53 @@
+const projectsData = [
+    {
+      id: 1,
+      image: "./images-projects/header.avif",
+      alt: "Riding Cities",
+      title: "Riding Cities",
+    },
+    {
+      id: 2,
+      image: "./images-projects/annie-spratt.avif",
+      alt: "Booki",
+      title: "Booki",
+    },
+    {
+      id: 3,
+      image: "./images-projects/colors.avif",
+      alt: "Print it",
+      title: "Print it",
+    },
+    {
+      id: 4,
+      image: "./images-projects/food.avif",
+      alt: "Ohmyfood",
+      title: "Ohmyfood",
+    },
+    {
+      id: 5,
+      image: "./images-projects/sophie-bluel.avif",
+      alt: "Sophie Bluel",
+      title: "Sophie Bluel",
+    },
+    {
+      id: 6,
+      image: "./images-projects/kasa.avif",
+      alt: "Kasa",
+      title: "Kasa",
+    },
+    {
+      id: 7,
+      image: "./images-projects/nina.avif",
+      alt: "Nina Carducci",
+      title: "Nina Carducci",
+    },
+    {
+      id: 8,
+      image: "./images-projects/bank-tree.avif",
+      alt: "Argent Bank",
+      title: "Argent Bank",
+    },
+  ];
+  
+  export default projectsData;
+  
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,42 +1,17 @@
 import React from "react";
+import projectsData from "../data/projectsData";
 
 function Projects() {
   return (
     <section id="projects">
       <h1>Projects</h1>
       <div className="projects-grid">
-        <div className="project-card">
-          <img src="./images-projects/SophieBluel.png" alt="Riding Cities" />
-          <p className="projects-paragraph">Riding Cities</p>
-        </div>
-        <div className="project-card">
-          <img src="./images-projects/SophieBluel.png" alt="Booki" />
-          <p className="projects-paragraph">Booki</p>
-        </div>
-        <div className="project-card">
-          <img src="./images-projects/SophieBluel.png" alt="Print it" />
-          <p className="projects-paragraph">Print it</p>
-        </div>
-        <div className="project-card">
-          <img src="./images-projects/Kasa-project.png" alt="Ohmyfood" />
-          <p className="projects-paragraph">Ohmyfood</p>
-        </div>
-        <div className="project-card">
-          <img src="path-to-image-03.jpg" alt="Sophie Bluel" />
-          <p className="projects-paragraph">Sophie Bluel</p>
-        </div>
-        <div className="project-card">
-          <img src="path-to-image-04.jpg" alt="Kasa" />
-          <p className="projects-paragraph">Kasa</p>
-        </div>
-        <div className="project-card">
-          <img src="path-to-image-04.jpg" alt="Nina Carducci" />
-          <p className="projects-paragraph">Nina Carducci</p>
-        </div>
-        <div className="project-card">
-          <img src="path-to-image-05.jpg" alt="Argent Bank" />
-          <p className="projects-paragraph">Argent Bank</p>
-        </div>
+        {projectsData.map((project) => (
+          <div key={project.id} className="project-card">
+            <img src={project.image} alt={project.alt} />
+            <p className="projects-paragraph">{project.title}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
