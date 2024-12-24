386e8fcd9ea2c5ea50918b860d7a5915d7e9701e
Dalila <dalila.fariello@gmail.com>
2024-12-24 14:57:13 +0100

    feat: added buttons for projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,6 +5,12 @@ function Projects() {
   return (
     <section id="projects">
       <h1>Projects</h1>
+      <div className="projects-buttons">
+        <button className="buttons">All</button>
+        <button className="buttons">JavaScript</button>
+        <button className="buttons">React</button>
+        <button className="buttons">Redux</button>
+      </div>
       <div className="projects-grid">
         {projectsData.map((project) => (
           <div key={project.id} className="project-card">
