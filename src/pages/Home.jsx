ecd6789c90ce7e99e435a3752d34d4e309b16de6
Dalila <dalila.fariello@gmail.com>
2024-12-28 01:28:10 +0100

    refactor: updated <p> tag to <h2> for improved semantic structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ function Home() {
   return (
     <div className="home-page">
       <h1 className="home-title">Dalila Fariello</h1>
-      <p className="home-paragraph">
+      <h2 className="home-paragraph">
         Developer of Web Interfaces and Front-End
-      </p>
+      </h2>
     </div>
   );
 }
