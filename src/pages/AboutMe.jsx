b773a641ee522ff8aba2b771760719e38b6586bb
Dalila <dalila.fariello@gmail.com>
2024-12-31 01:01:54 +0100

    feat: improve about-me section structure with semantic HTML

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,20 +5,27 @@ function AboutMe() {
     <div className="about-me-div">
       <section id="about-me">
         <h1 className="about-me-title">About Me</h1>
-        <p className="about-me-paragraph">
-          I am a passionate front-end developer and lifelong learner who thrives
-          on creating engaging web experiences.
-          <br />
-          Fluent in Italian, Spanish, English, and French, I love connecting
-          with people from diverse cultures and continuously expanding my
-          horizons.
-          <br />A travel enthusiast and dog lover, I enjoy exploring new places,
-          embracing different languages, and enriching my perspective through
-          learning and collaboration.
-          <br /> In my free time, I indulge in my love for theater, staying
-          active at the gym, and discovering new cuisines—because great food is
-          an adventure in itself!
-        </p>
+        <div className="text-content">
+          <p className="about-me-paragraph">
+            I am a passionate front-end developer and lifelong learner who
+            thrives on creating engaging web experiences.
+          </p>
+          <p className="about-me-paragraph">
+            Fluent in Italian, Spanish, English, and French, I love connecting
+            with people from diverse cultures and continuously expanding my
+            horizons.
+          </p>
+          <p className="about-me-paragraph">
+            A travel enthusiast and dog lover, I enjoy exploring new places,
+            embracing different languages, and enriching my perspective through
+            learning and collaboration.
+          </p>
+          <p className="about-me-paragraph">
+            In my free time, I indulge in my love for theater, staying active at
+            the gym, and discovering new cuisines—because great food is an
+            adventure in itself!
+          </p>
+        </div>
       </section>
       <section className="image-section">
         <img
